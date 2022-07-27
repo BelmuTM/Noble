ee7ea233142aa16c24aed6898200b8fd0ab7d151
Belmu <belmuledozo@gmail.com>
2022-07-27 23:18:20 +0200

    scoopy poopy

--- shaders/programs/composite/composite0.glsl
+++ shaders/programs/composite/composite0.glsl
@@ -36,21 +36,23 @@
 void main() {
     color = texture(colortex4, texCoords).rgb;
 
-    #if GI == 0 && REFLECTIONS == 1
-        if(clamp(texCoords, vec2(0.0), vec2(REFLECTIONS_RES)) == texCoords) {
-            vec2 scaledUv  = texCoords * rcp(REFLECTIONS_RES);
+    #if GI == 0
+        #if REFLECTIONS == 1
+            if(clamp(texCoords, vec2(0.0), vec2(REFLECTIONS_RES)) == texCoords) {
+                vec2 scaledUv  = texCoords * rcp(REFLECTIONS_RES);
 
-            if(!isSky(scaledUv)) {
-                vec3 viewPos = getViewPos0(scaledUv);
-                Material mat = getMaterial(scaledUv);
+                if(!isSky(scaledUv)) {
+                    vec3 viewPos = getViewPos0(scaledUv);
+                    Material mat = getMaterial(scaledUv);
                     
-                #if REFLECTIONS_TYPE == 1
-                    reflections = roughReflections(viewPos, mat);
-                #else
-                    reflections = simpleReflections(viewPos, mat);
-                #endif
+                    #if REFLECTIONS_TYPE == 1
+                        reflections = roughReflections(viewPos, mat);
+                    #else
+                        reflections = simpleReflections(viewPos, mat);
+                    #endif
+                }
             }
-        }
+        #endif
     #else
         #if GI_FILTER == 1
             aTrousFilter(color, colortex4, texCoords, moments, 3);
