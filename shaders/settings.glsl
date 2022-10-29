dedd71eba09a87c8fbc98c0eb602bafbc6345942
Belmu <belmuledozo@gmail.com>
2022-10-29 18:15:11 +0200

    Tweaked max exposure

--- shaders/settings.glsl
+++ shaders/settings.glsl
@@ -181,7 +181,7 @@ const float hardCodedRoughness = 0.0; // 0.0 = OFF
 #define FOG_DENSITY   0.00 // [0.00 0.05 0.10 0.15 0.20 0.25 0.30 0.35 0.40 0.45 0.50 0.55 0.60 0.65 0.70 0.75 0.80 0.85 0.90 0.95 1.00]
 
 #define STARS_AMOUNT      0.10
-#define STARS_BRIGHTNESS   4.0
+#define STARS_BRIGHTNESS   6.0
 #define STARS_MIN_TEMP  2500.0
 #define STARS_MAX_TEMP 50000.0
 
