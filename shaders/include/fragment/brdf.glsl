4edea639774482011b9c337dfba1a99cf288f6d0
Belmu <belmuledozo@gmail.com>
2023-05-13 08:37:39 +0200

    Increased ambient lighting in the Nether

--- shaders/include/fragment/brdf.glsl
+++ shaders/include/fragment/brdf.glsl
@@ -180,9 +180,9 @@ vec3 computeDiffuse(vec3 viewDirection, vec3 lightDirection, Material material, 
     vec3 emissiveness    = material.emission * blocklightColor;
 
     #if defined WORLD_OVERWORLD || defined WORLD_END
-        vec3 ambient = vec3(0.1);
+        vec3 ambient = vec3(0.2);
     #else
-        vec3 ambient = vec3(0.5);
+        vec3 ambient = vec3(1.0);
     #endif
 
     diffuse += (blocklight + skylight + ambient) * material.ao * ao;
