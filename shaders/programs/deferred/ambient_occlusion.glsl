90c80100bc9d0e8e9aea00de99bf991d435140ef
Belmu <belmuledozo@gmail.com>
2024-04-08 18:48:39 +0200

    Fixed AO applying on player hand

--- shaders/programs/deferred/ambient_occlusion.glsl
+++ shaders/programs/deferred/ambient_occlusion.glsl
@@ -161,7 +161,7 @@
 				}
 			#endif
 
-			if(depth == 1.0) return;
+			if(depth == 1.0 || depth < handDepth) return;
 
 			Material material = getMaterial(vertexCoords);
 			vec3 viewPosition = screenToView(vec3(textureCoords, depth), projectionInverse, true);
@@ -182,7 +182,7 @@
 
 				vec3 prevAO = texture(AO_BUFFER, prevCoords).rgb;
 		
-				float weight = 1.0 / clamp(texture(ACCUMULATION_BUFFER, prevCoords).a * float(depth >= handDepth), 1.0, 64.0);
+				float weight = 1.0 / clamp(texture(ACCUMULATION_BUFFER, prevCoords).a, 1.0, 64.0);
 
 				#if AO == 1 || AO == 3
 					vec3 prevBentNormal = decodeUnitVector(prevAO.xy);
