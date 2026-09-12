5fdd6a8a0ea03a6becff05a2f40013c02503f6fd
Belmu <belmuledozo@gmail.com>
2026-09-12 08:14:19 +0200

    Attempt at fixing NVIDIA color space issue

--- shaders/include/utility/color.glsl
+++ shaders/include/utility/color.glsl
@@ -157,11 +157,13 @@ vec3 srgbToLinearAlbedoAP1(vec3 color) {
     return srgbToLinear(color) * SRGB_2_AP1_ALBEDO;
 }
 
-#define SRGB_TO_WORKING_SPACE(SRGB_COLOR) \
-    srgbToLinearAlbedoAP1(SRGB_COLOR)
+vec3 SRGB_TO_WORKING_SPACE(vec3 srgb) {
+    return srgbToLinearAlbedoAP1(srgb);
+}
 
-#define SRGB_TO_WORKING_SPACE_ALBEDO(ALBEDO_SRGB_COLOR) \
-    (ALBEDO_SRGB_COLOR) * SRGB_2_AP1_ALBEDO
+vec3 SRGB_TO_WORKING_SPACE_ALBEDO(vec3 srgb) {
+    return srgb * SRGB_2_AP1_ALBEDO;
+}
 
 vec3 fromYCoCg(vec3 color) {
     float r = color.x + color.y - color.z;
